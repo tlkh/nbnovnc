2bd669f1c94f729889aa57122579e38754cfe601
Martin Hunt <huntmartinm@gmail.com>
2018-12-18 11:59:43 -0500

    remove supervisor call, simplify

--- nbnovnc/handlers.py
+++ nbnovnc/handlers.py
@@ -1,92 +1,14 @@
-import configparser
-import os
-import socket
-import tempfile
-
 from notebook.utils import url_path_join as ujoin
-
-from traitlets import Unicode, Integer
-from traitlets.config.configurable import Configurable
-
 from nbserverproxy.handlers import AddSlashHandler, SuperviseAndProxyHandler
 
-class SupervisorHandler(SuperviseAndProxyHandler):
-    '''Supervise supervisord.'''
-
-    name = 'supervisord'
-
-    def supervisor_config(self):
-        config = configparser.ConfigParser()
-        config['supervisord'] = {'nodaemon': 'true'}
-        config['inet_http_server'] = {'port': '127.0.0.1:9001'}
-        config['rpcinterface:supervisor'] = {'supervisor.rpcinterface_factory': 'supervisor.rpcinterface:make_main_rpcinterface'}
-        return config
-
-    def write_conf(self):
-        config = self.supervisor_config()
-        f = tempfile.NamedTemporaryFile(mode='w', delete=False)
-        config.write(f)
-        f.close()
-        return f.name
-
-    def get_cmd(self):
-        filename = self.write_conf()
-        return ["supervisord", "-c", filename, "--nodaemon"]
-
-class NBNoVNC(Configurable):
-    geometry = Unicode(u"1024x768", help="Desktop geometry.", config=True)
-    depth = Integer(24, help="Desktop display depth.", config=True)
-    novnc_directory = Unicode(u"/apps/share64/debian7/noVNC", config=True,
-        help="Path to noVNC web assets.")
-    vnc_command = Unicode(u"xinit -- /usr/bin/Xtigervnc :{display} -geometry {geometry} -depth {depth}", config=True,
-        help="Command to start VNC server. Contains string replacement fields.")
-    websockify_command = Unicode(u"/apps/share64/debian7/noVNC/utils/websockify/websockify.py --web {novnc_directory} --heartbeat {heartbeat} {port} localhost:{vnc_port}", config=True,
-        help="websockify command. Contains string replacement fields.")
-
-class NoVNCHandler(SupervisorHandler):
+class NoVNCHandler(SuperviseAndProxyHandler):
     '''Supervise novnc, websockify, and a VNC server.'''
     def initialize(self, state):
         # print("NoVNCHandler Init: %s" % state)
         super().initialize(state)
-        self.c = NBNoVNC(config=self.config)
-        # This is racy because we don't immediately start the VNC server.
-        self.vnc_port = self.random_empty_port()
 
-    def random_empty_port(self):
-        '''Allocate a random empty port for use by the VNC server.'''
-        sock = socket.socket()
-        sock.bind(('', 0))
-        port = sock.getsockname()[1]
-        sock.close()
-        return port
-
-    @property
-    def display(self):
-        return self.vnc_port-5900
-
-    def get_env(self):
-        return { 'DISPLAY': ':' + str(self.display) }
-
-    def supervisor_config(self):
-        config = super().supervisor_config()
-        config['program:vnc'] = {
-            'command': self.c.vnc_command.format(
-                display=self.display,
-                geometry=self.c.geometry,
-                depth=self.c.depth
-            ),
-            'priority': 10,
-        }
-        config['program:websockify'] = {
-            'command': self.c.websockify_command.format(
-                novnc_directory=self.c.novnc_directory,
-                heartbeat=30,
-                port=self.port,
-                vnc_port=self.vnc_port
-            ),
-            'priority': 20,
-        }
-        return config
+    def get_cmd(self):
+        return ['/apps/share64/debian7/noVNC/startNB']
 
     async def get(self, path):
         '''
@@ -108,5 +30,3 @@ def setup_handlers(web_app):
             dict(state={})),
         (ujoin(web_app.settings['base_url'], 'novnc'), AddSlashHandler)
     ])
-
-# vim: set et ts=4 sw=4:
