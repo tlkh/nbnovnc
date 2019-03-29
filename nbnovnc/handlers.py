5e5f01ea18b59f30e6e27443a3d09de3cf7dfee4
apps <clarks@purdue.edu>
2019-03-29 12:46:44 -0400

    use jupyter-server-proxy

--- nbnovnc/handlers.py
+++ nbnovnc/handlers.py
@@ -1,6 +1,6 @@
 import os
 from notebook.utils import url_path_join as ujoin
-from nbserverproxy.handlers import AddSlashHandler, SuperviseAndProxyHandler
+from jupyter_server_proxy.handlers import AddSlashHandler, SuperviseAndProxyHandler
 
 class NoVNCHandler(SuperviseAndProxyHandler):
     '''Supervise novnc, websockify, and a VNC server.'''
