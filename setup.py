4f135cd83e5cbfbf61472ab4147704139224d031
Timothy Liu <tlkh@live.co.uk>
2019-04-03 03:00:39 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,9 @@ setuptools.setup(
     author_email="rylo@berkeley.edu",
     description="Jupyter extension to proxy a VNC session via noVNC",
     packages=setuptools.find_packages(),
-	keywords=['Jupyter'],
-	classifiers=['Framework :: Jupyter'],
+    include_package_data=True,
+    keywords=['Jupyter'],
+    classifiers=['Framework :: Jupyter'],
     install_requires=[
         'pyzmq >= 17',
         'tornado == 5.1.1',
